9aebc312e39dca48cc877ae603d98aa33997c6b8
Tianming Wu <tianming.wu@utexas.edu>
2026-08-24 00:36:20 +0000

    update sift10m/100m scripts

--- benchmark/datasets/sift100m.sh
+++ benchmark/datasets/sift100m.sh
@@ -16,12 +16,14 @@ DISKANN_R="${DISKANN_R:-64}"
 __dataset_root="${SIFT100M_ROOT:-/path/to/datasets/sift100m}"
 __index_root="${SIFT100M_INDEX_DIR:-/path/to/indexes/sift100m}"
 
-# TODO: Configure the source vector files.
-DATA_BVECS="${DATA_BVECS:-$__dataset_root/base.100M.bvecs}"
-QUERY_BVECS="${QUERY_BVECS:-$__dataset_root/query.10K.bvecs}"
-
-# Generated artifact paths; no configuration needed.
+# TODO: Configure the .bin input vector files.
 DATA_BIN="${DATA_BIN:-$__dataset_root/base.100M.bin}"
 QUERY_BIN="${QUERY_BIN:-$__dataset_root/query.10K.bin}"
+
+# Alternatively, uncomment these to convert bvecs inputs to the paths above.
+# DATA_BVECS="${DATA_BVECS:-$__dataset_root/base.100M.bvecs}"
+# QUERY_BVECS="${QUERY_BVECS:-$__dataset_root/query.10K.bvecs}"
+
+# Generated artifact paths; no configuration needed.
 GT_BIN_KCACHED="${GT_BIN_KCACHED:-$__dataset_root/sift100m_100000000_gt100.bin}"
 DISKANN_INDEX_0P9B="${DISKANN_INDEX_0P9B:-$__index_root/sift100m_90M_R64_L100_B32_M8_noPQ}"
--- benchmark/datasets/sift10m.sh
+++ benchmark/datasets/sift10m.sh
@@ -16,12 +16,14 @@ DISKANN_R="${DISKANN_R:-64}"
 __dataset_root="${SIFT10M_ROOT:-/path/to/datasets/sift10m}"
 __index_root="${SIFT10M_INDEX_DIR:-/path/to/indexes/sift10m}"
 
-# TODO: Configure the source vector files.
-DATA_BVECS="${DATA_BVECS:-$__dataset_root/base.10M.bvecs}"
-QUERY_BVECS="${QUERY_BVECS:-$__dataset_root/query.10K.bvecs}"
-
-# Generated artifact paths; no configuration needed.
+# TODO: Configure the .bin input vector files.
 DATA_BIN="${DATA_BIN:-$__dataset_root/base.10M.bin}"
 QUERY_BIN="${QUERY_BIN:-$__dataset_root/query.10K.bin}"
+
+# Alternatively, uncomment these to convert bvecs inputs to the paths above.
+# DATA_BVECS="${DATA_BVECS:-$__dataset_root/base.10M.bvecs}"
+# QUERY_BVECS="${QUERY_BVECS:-$__dataset_root/query.10K.bvecs}"
+
+# Generated artifact paths; no configuration needed.
 GT_BIN_KCACHED="${GT_BIN_KCACHED:-$__dataset_root/sift10m_10000000_gt100.bin}"
 DISKANN_INDEX_0P9B="${DISKANN_INDEX_0P9B:-$__index_root/sift10m_9M_R64_L100_B32_M8_noPQ}"
